99683da8066e36361c9f882948400d1cb29614b5
AnnabelleEllis <ellisac@jncb.com>
2022-06-26 00:38:50 -0500

    deleting the old src and adding a now src with index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+// in every react application we have to mout the application on the root element
+ReactDOM.render(<App />, document.getElementById('root'));
